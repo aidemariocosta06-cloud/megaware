4b90be003b6ceac707cc5480971144b734df4c66
GITAMO2004 <tavaregigimoba@gmail.com>
2026-05-30 14:17:31 +0100

    update project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import { Hero } from '@/components/sections/hero'
+import { Services } from '@/components/sections/services'
+import { Shop } from '@/components/sections/shop'
+import { Promotions } from '@/components/sections/promotions'
+import { About } from '@/components/sections/about'
+import { Testimonials } from '@/components/sections/testimonials'
+import { Contacts } from '@/components/sections/contacts'
 
 export default function HomePage() {
   return (
-    <div className="pt-20">
+    <>
       <Hero />
-    </div>
+      <Services />
+      <Shop />
+      <Promotions />
+      <About />
+      <Testimonials />
+      <Contacts />
+    </>
   )
 }
